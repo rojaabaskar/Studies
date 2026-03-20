ada101307c63cc0c60b50118159be0346a46d17e
roja <rojabaskar132@gmail.com>
2026-03-20 23:11:14 +0530

    2nd commit

--- practice.py
+++ practice.py
@@ -1,10 +1,13 @@
 urls = ["https://google.com", "https://github.com", "https://invalid-url.com"]
      
-def Check_urls(urls):
+def link(urls):
     n = 1 
     for link in urls:
         
-        print(str(n)+".", link)
+        print(str(n)+".",urls)
         n= n+1
     
-Check_urls(urls)
+    return(n)
+
+check_urls= link(urls)
+print(check_urls)
