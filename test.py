61c26d8c17c7042b5dd3c3eec4eb9032c1c8ad65
roja <rojabaskar132@gmail.com>
2026-03-24 21:02:20 +0530

    3rd commit

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 urls = ["https://google.com", "https://github.com", "https://invalid-url.com"]
      
 def Check_urls(urls):
-    n = 1 
-    for link in urls:
-        
-        print(str(n)+".", link)
-        n= n+1
-    
-Check_urls(urls)
+    if len(urls)==0:                    # condition to check the empty list 
+        print("No URLs provided")
+    else: 
+        n = 1 
+        for link in urls:               # loop through each URL
+            print(str(n)+".", link)
+            n= n+1
+
+Check_urls(["", "https://github.com", ""]) # calling the fucntion with a list along with few blank data
